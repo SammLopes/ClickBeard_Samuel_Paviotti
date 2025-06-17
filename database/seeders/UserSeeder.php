8a5d7911626f33b16b5684992670a7f9296ddab1
SammLopes <sam.v.p.lopes@gmail.com>
2025-06-17 15:34:43 -0300

    Adicoinado seeder do admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Carbon;
 use App\Models\User;
 
 class UserSeeder extends Seeder
@@ -13,10 +15,13 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-          User::create([
-            'name' => 'Samuel Paviotti',
-            'email' => 'samuel@clickbeard.com',
-            'password' => app('hash')->make('123456')
+        User::create([
+                'name' => 'Admin',
+                'email' => 'admin@barbearia.com',
+                'password' => Hash::make('1234'), 
+                'role' => 'admin',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
         ]);
     }
 }
